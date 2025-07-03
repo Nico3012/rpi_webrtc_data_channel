8f6f52633a9d1f96a2946c295899e7f5bcbb9417
Nico Hörter <nico.m.hoerter@gmail.com>
2025-07-03 13:20:29 +0200

    small test improvements

--- arduino/servo/servo.ino
+++ arduino/servo/servo.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
 // Configuration
-const int SERVO_PIN = 9;
+const int SERVO_PIN = 3;
 const int MIN_DUTY_CYCLE = 500; // microseconds (1ms)
 const int MAX_DUTY_CYCLE = 2500; // microseconds (2ms)
 
@@ -17,13 +17,7 @@ void loop() {
     myServo.writeMicroseconds(map(pos, 0, 100, MIN_DUTY_CYCLE, MAX_DUTY_CYCLE));
     delay(500);
   }
-  
-  // Move from 100% back to 0% in 10% steps
-  for (int pos = 100; pos >= 0; pos -= 10) {
-    myServo.writeMicroseconds(map(pos, 0, 100, MIN_DUTY_CYCLE, MAX_DUTY_CYCLE));
-    delay(500);
-  }
-  
+
   // Wait for 2 seconds at the end
   delay(2000);
 }
