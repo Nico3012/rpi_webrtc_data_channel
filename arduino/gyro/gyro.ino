e0dd5bc29247539733d17988d391db3872559b39
Nico Hörter <nico.m.hoerter@gmail.com>
2025-07-03 14:55:06 +0200

    Update and make it work

--- arduino/gyro/gyro.ino
+++ arduino/gyro/gyro.ino
@@ -1,20 +1,20 @@
 #include<Wire.h>
-const int MPU=0x68; 
+
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
 void  setup(){
   Wire.begin();
-  Wire.beginTransmission(MPU);
+  Wire.beginTransmission(0x68);
   Wire.write(0x6B);  
   Wire.write(0);    
   Wire.endTransmission(true);
   Serial.begin(9600);
 }
 void  loop(){
-  Wire.beginTransmission(MPU);
+  Wire.beginTransmission(0x68);
   Wire.write(0x3B);  
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU,12,true);  
+  Wire.requestFrom(0x68,12,true);  
   AcX=Wire.read()<<8|Wire.read();    
   AcY=Wire.read()<<8|Wire.read();  
   AcZ=Wire.read()<<8|Wire.read();  
