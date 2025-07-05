781b3a0e063fbe55766de5ea68842ae8ce16ccac
Nico Hörter <nico.m.hoerter@gmail.com>
2025-07-05 23:01:25 +0200

    implement sample server, that does not rely on servo and sense package

--- rpi/main.go
+++ rpi/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"strconv"
 	"time"
 
-	"rpi-webrtc/sense"
-	"rpi-webrtc/servohardware"
 	"rpi-webrtc/webrtcserver"
 )
 
@@ -15,46 +12,21 @@ func main() {
 	// init web connection
 	server := webrtcserver.New("8080", "webrtcserver/public")
 
-	// init servo
-	servoController, err := servohardware.New("GPIO18", 50, 500, 2500)
-	if err != nil {
-		log.Fatalf("Failed to initialize servo: %v", err)
-	}
-	defer servoController.Close()
-
-	// init sensor
-	sensor, err := sense.New()
-	if err != nil {
-		log.Fatalf("Failed to initialize sensor: %v", err)
-	}
-	defer sensor.Close()
-
 	// web message callback
 	server.InitReadDataCallback(func(message string) {
-		position, err := strconv.ParseFloat(message, 64)
-		if err != nil {
-			log.Fatalf("Failed to parse float: %v", err)
-		}
-
-		err = servoController.Write(position)
-		if err != nil {
-			log.Fatalf("Failed to write servo position: %v", err)
-		}
+		log.Printf("Nico msg: %s\n", message)
 	})
 
-	for {
-		if server.IsConnected() {
-			pitch, roll, err := sensor.Read()
-			if err != nil {
-				log.Fatalf("Failed to read sensor: %v", err)
-			}
+	i := 0
 
-			err = server.SendData(fmt.Sprintf("%.2f;%.2f", pitch, roll))
-			if err != nil {
-				log.Fatalf("Failed to send data: %v", err)
-			}
+	for {
+		err := server.SendData(fmt.Sprintf("%d;%d", i, i*i))
+		if err != nil {
+			log.Println(err)
 		}
 
-		time.Sleep(10 * time.Millisecond)
+		i++
+
+		time.Sleep(2000 * time.Millisecond)
 	}
 }
