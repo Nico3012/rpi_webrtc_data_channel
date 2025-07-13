8b2adb02618c85ab698a5cc2bfb6a6add41af33f
Nico Hörter <nico.m.hoerter@gmail.com>
2025-07-13 13:58:00 +0200

    remove ogg and ivf debug files

--- rpi/webrtcserver/audio/cmd.sh
+++
@@ -1,8 +0,0 @@
-ffmpeg \
--f pulse -i default \ # use linux pulse audio with default device
--c:a libopus \ # set audio codec to opus
--page_duration 20000 \
--vn \ # disable video
--f ogg pipe:1 # pipe out as ogg
-
-# for testing: replace pipe:1 with output.ogg
