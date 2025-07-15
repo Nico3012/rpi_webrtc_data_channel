7f7c14ccc224591736486a2d24a3576501d83126
Nico Hörter <nico.m.hoerter@gmail.com>
2025-07-15 15:33:53 +0200

    build main.go as router to push messages through the arduino

--- rpi/main.go
+++ rpi/main.go
@@ -1,55 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"rpi-webrtc/serialcomm"
-	"time"
+	"rpi-webrtc/webrtcserver"
 )
 
 func main() {
-	// init web connection with both video and audio enabled
-	// server := webrtcserver.New("8080", "webrtcserver/public", true, true)
-
-	// // web message callback
-	// server.InitReadDataCallback(func(message string) {
-	// 	log.Printf("Received message: %s\n", message)
-
-	// 	// You could handle specific commands here
-	// 	// For example, if message == "video:off" { server.EnableVideo(false) }
-	// })
-
-	// i := 0
-
-	// for {
-	// 	// Send some data to the client periodically
-	// 	if server.IsConnected() {
-	// 		err := server.SendData(fmt.Sprintf("%d;%d", i, i*i))
-	// 		if err != nil {
-	// 			log.Println(err)
-	// 		}
-	// 		i++
-	// 	}
-
-	// 	time.Sleep(2000 * time.Millisecond)
-	// }
-
+	server := webrtcserver.New("8080", "webrtcserver/public", true, true)
 	port, err := serialcomm.NewAuto(9600)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
-	port.InitDataCallback(func(s string) {
-		log.Printf("Received from arduino: %s\n", s)
+	server.InitReadDataCallback(func(data string) {
+		err := port.SendData(data)
+		if err != nil {
+			log.Println(err)
+		}
 	})
 
-	i := 0
-
-	for {
-		port.SendData(fmt.Sprintf("%d", i))
-
-		i++
-
-		time.Sleep(2000 * time.Millisecond)
-	}
+	port.InitDataCallback(func(data string) {
+		err := server.SendData(data)
+		if err != nil {
+			log.Println(err)
+		}
+	})
 }
