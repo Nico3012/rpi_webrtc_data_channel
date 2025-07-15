bcfe366b3eb9ff22302c3c44e5ab48b3b6de498d
Nico Hörter <nico.m.hoerter@gmail.com>
2025-07-15 16:40:41 +0200

    create auto initialization pipeline

--- rpi/main.go
+++ rpi/main.go
@@ -4,13 +4,23 @@ import (
 	"log"
 	"rpi-webrtc/serialcomm"
 	"rpi-webrtc/webrtcserver"
+	"time"
 )
 
 func main() {
 	server := webrtcserver.New("8080", "webrtcserver/public", true, true)
-	port, err := serialcomm.NewAuto(9600)
-	if err != nil {
-		log.Fatalln(err)
+	var port *serialcomm.Port
+	var err error
+
+	for {
+		port, err = serialcomm.NewAuto(9600)
+		if err == nil {
+			break
+		}
+
+		log.Println(err)
+		time.Sleep(2 * time.Second)
+		log.Println("Try initialization again...")
 	}
 
 	server.InitReadDataCallback(func(data string) {
