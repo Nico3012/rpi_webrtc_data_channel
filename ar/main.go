d0ccf8a2bad7e9ea66dc038a9447cb6a22867310
Nico Hörter <nico.m.hoerter@gmail.com>
2025-07-29 10:58:22 +0200

    copy AlvaAR examples to project

---
+++ ar/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// addCacheControlNoCache is a middleware that adds Cache-Control: no-cache header to every response
+func addCacheControlNoCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	log.Println("Starting web server on :443")
+	fileServer := http.FileServer(http.Dir("public"))
+
+	// Wrap the file server with our cache control middleware
+	handler := addCacheControlNoCache(fileServer)
+
+	log.Fatalln(http.ListenAndServeTLS(":443", "cert.pem", "cert_key.pem", handler))
+}
