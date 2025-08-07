319dd864249c94a333a9c43fc23cf53264b52a64
Nico Hörter <nico.m.hoerter@gmail.com>
2025-08-07 22:57:02 +0200

    add sample files for public directory

---
+++ service-worker/v2/public/script.js
@@ -0,0 +1,13 @@
+import { installSW, uninstallSW, isInstalled } from '/api/files/script.js';
+
+if (!isInstalled()) {
+    await installSW();
+    // alert('installed');
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', async () => {
+    await uninstallSW();
+    alert('uninstalled, reloading now.');
+    location.reload();
+});
