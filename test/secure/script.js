0793cfb18bc3a6bf539d4d50827e811b9eef5c3d
Nico Hörter <nico.m.hoerter@gmail.com>
2025-08-22 00:36:34 +0200

    change to new api

--- test/secure/script.js
+++ test/secure/script.js
@@ -1,6 +1,6 @@
 const handshakeManager = document.querySelector('handshake-manager');
 
-handshakeManager.setOffer('abc_offer_stuff');
+handshakeManager.offer = 'abc_offer_stuff';
 handshakeManager.addEventListener('answer-received', event => {
     console.log(event.detail.answer);
 });
