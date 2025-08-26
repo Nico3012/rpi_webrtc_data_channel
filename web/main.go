b748fdc8e6bf2a6e7f9f4927e5fd4e3691b9a1a7
Nico Hörter <nico.m.hoerter@gmail.com>
2025-08-26 11:51:31 +0200

    change to http service for proxy compatibility

--- web/main.go
+++ web/main.go
@@ -6,5 +6,6 @@ import (
 )
 
 func main() {
-	log.Fatalln(cacheserver.StartSecureServer(":8443", "cert.pem", "cert_key.pem", ".", "public"))
+	// log.Fatalln(cacheserver.StartSecureServer(":8443", "cert.pem", "cert_key.pem", ".", "public"))
+	log.Fatalln(cacheserver.StartServer(":8080", ".", "public"))
 }
