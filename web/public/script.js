097a5b087cac014476f0ea38703d491ab8048fe8
Nico Hörter <nico.m.hoerter@gmail.com>
2025-08-26 11:12:17 +0200

    change logging

--- web/public/script.js
+++ web/public/script.js
@@ -14,6 +14,10 @@ webrtcConnection.addEventListener('connection-update', () => {
     microphoneElement.srcObject = audioStream;
 });
 
+webrtcConnection.addEventListener('message-received', (event) => {
+    console.log('Message received:', event.detail.message);
+});
+
 let speed = 0; //  0 - 1
 let angle = 0; // -1 - 1
 
@@ -21,10 +25,10 @@ leftJoystick.addEventListener('stick-move', (event) => {
     speed = (event.detail.y + 1) / 2; // assign speed
 
     if (webrtcConnection.isConnected()) {
-        console.log(`${speed.toFixed(2)};${angle.toFixed(2)}`);
+        // console.log(`${speed.toFixed(2)};${angle.toFixed(2)}`);
         webrtcConnection.sendData(`${speed.toFixed(2)};${angle.toFixed(2)}`);
     } else {
-        console.log('not connected:', `${speed.toFixed(2)};${angle.toFixed(2)}`);
+        // console.log('not connected:', `${speed.toFixed(2)};${angle.toFixed(2)}`);
     }
 });
 
@@ -32,10 +36,10 @@ rightJoystick.addEventListener('stick-move', (event) => {
     angle = event.detail.x; // assign angle
 
     if (webrtcConnection.isConnected()) {
-        console.log(`${speed.toFixed(2)};${angle.toFixed(2)}`);
+        // console.log(`${speed.toFixed(2)};${angle.toFixed(2)}`);
         webrtcConnection.sendData(`${speed.toFixed(2)};${angle.toFixed(2)}`);
     } else {
-        console.log('not connected:', `${speed.toFixed(2)};${angle.toFixed(2)}`);
+        // console.log('not connected:', `${speed.toFixed(2)};${angle.toFixed(2)}`);
     }
 });
 
