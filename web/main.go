0c0f370696055e94f6a111371e59fef87354f9bf
Nico Hörter <nico.m.hoerter@gmail.com>
2026-01-05 21:13:43 +0100

    add seperated production mode using environment variable

--- web/main.go
+++ web/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/Nico3012/rpi_webrtc_data_channel/web/internal/cacheserver"
 )
 
 func main() {
-	log.Fatalln(cacheserver.StartSecureServer(":8443", "cert.pem", "cert_key.pem", ".", "public"))
-	// log.Fatalln(cacheserver.StartServer(":8080", ".", "public"))
+	if os.Getenv("PRODUCTION_MODE") == "1" {
+		log.Fatalln(cacheserver.StartServer(":80", ".", "public"))
+	} else {
+		log.Fatalln(cacheserver.StartSecureServer(":8443", "cert.pem", "cert_key.pem", ".", "public"))
+	}
 }
