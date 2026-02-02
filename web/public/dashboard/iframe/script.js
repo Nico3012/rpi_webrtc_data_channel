9d88622ee3e53a271faf0ebc6fd30842fee69fdd
Nico Hörter <nico.m.hoerter@gmail.com>
2026-02-03 00:23:34 +0100

    add rotating heading example

--- web/public/dashboard/iframe/script.js
+++ web/public/dashboard/iframe/script.js
@@ -12,3 +12,35 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 
 tiles.addTo(map);
+
+{ // sample data injection
+    let speed = 5; // m/s
+    let heading = 90 // west
+    let lat = 51.505; // deg
+    let lon = -0.09; // deg
+
+    const img = document.createElement('img');
+    img.className = 'marker-image';
+    img.src = `../heading-foreground.svg`;
+    /** @param {number} heading */
+    const setMarkerHeading = (heading) => {
+        img.style.transform = `rotate(-${heading.toString()}deg)`;
+    };
+
+    setMarkerHeading(heading);
+
+    const divIcon = L.divIcon({
+        className: 'icon-marker-container',
+        html: img,
+        iconSize: [40, 40],
+    });
+
+    const marker = L.marker([51.5, -0.09], {icon: divIcon});
+
+    marker.addTo(map);
+
+    setInterval(() => {
+        heading += 5;
+        setMarkerHeading(heading);
+    }, 200);
+}
