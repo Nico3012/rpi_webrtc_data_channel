803249101259453b66086da53b61f80fb96acc23
Nico Hörter <nico.m.hoerter@gmail.com>
2026-03-11 15:35:08 +0100

    add environment variable configuration instructions

--- rpi/controller/main.go
+++ rpi/controller/main.go
@@ -1,5 +1,8 @@
 package main
 
+// set VIDEO_MODE=windows-privat # can also be empty, then dummy video is used
+// set AUDIO_MODE=windows-privat # can also be empty, then dummy audio is used
+
 import "github.com/Nico3012/rpi_webrtc_data_channel/rpi/controller/internal/webrtcserver"
 
 func main() {
