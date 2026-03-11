581d7a4462583377de585adabe1ef1819ad4b616
Nico Hörter <nico.m.hoerter@gmail.com>
2026-03-11 10:23:04 +0100

    change code

--- lego_powerfunctions_ir_arduino/main.ino
+++ lego_powerfunctions_ir_arduino/main.ino
@@ -10,9 +10,9 @@ void loop() {
   // PWM_FWD7 is full speed (1-7)
   // PWM_FLT is stop (strom weg (ausrollen))
   // PWM_REV7 is full reverse speed (1-7)
-  lego.SingleOutput(0, PWM_FWD4, BLUE, CH1); // stufe 4/7 forwärts
+  lego.SingleOutput(0, PWM_FWD2, BLUE, CH1); // stufe 2/7 forwärts
   delay(4000);
-  lego.SingleOutput(0, PWM_FWD2, BLUE, CH1); // stufe 4/7 forwärts
+  lego.SingleOutput(0, PWM_FWD4, BLUE, CH1); // stufe 4/7 forwärts
   delay(4000);
   lego.SingleOutput(0, PWM_FLT, BLUE, CH1); // strom weg -> ausrollen
   delay(8000);
